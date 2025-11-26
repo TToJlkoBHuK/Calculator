8b2004b9626b1597863378047d7f56f0a988e877
TToJlkoBHuK <28arhalex@gmail.com>
2025-11-26 10:51:14 +0300

    update loader.cpp
    The logic of uploading a specific file has been implemented to remove nesting in the loop.

--- src/loader.cpp
+++ src/loader.cpp
@@ -2,44 +2,48 @@
 #include "Register.h"
 #include "PluginAdapter.h"
 
+void tryLoadPlugin(const std::wstring& fullPath, Register& registry) {
+    HMODULE Module = LoadLibrary(fullPath.c_str());
+    if (!Module) {
+        std::wcerr << L"not load function " << fullPath.c_str() << std::endl;
+        return;
+    }
+
+    FuncPtr func = (FuncPtr)GetProcAddress(Module, "operationE");
+    FuncNamePtr name = (FuncNamePtr)GetProcAddress(Module, "getNameE");
+    FuncArityPtr arity = (FuncArityPtr)GetProcAddress(Module, "getArityE");
+
+    if (func && name && arity) {
+        std::string namefunc = name();
+        int arityfunc = arity();
+
+        registry.addOper(std::make_unique<PluginAdapter>(namefunc, func, arityfunc, Module));
+        std::cout << "loaded " << namefunc << " arity " << arityfunc << std::endl;
+    }
+    else {
+        std::wcerr << L"not load function from " << fullPath.c_str() << L" invalid DLL" << std::endl;
+        FreeLibrary(Module);
+    }
+}
+
 void loader(Register& registry) {
-	WIN32_FIND_DATA FindFile;
-	wchar_t current[MAX_PATH];
-	GetCurrentDirectoryW(MAX_PATH, current);
+    WIN32_FIND_DATA FindFile;
+    wchar_t current[MAX_PATH];
+    GetCurrentDirectoryW(MAX_PATH, current);
 
-	std::wstring PlaginPath = std::wstring(current) + L"/plugins/*.dll";
-	HANDLE hFile = FindFirstFile(PlaginPath.c_str(), &FindFile);
+    std::wstring PluginPath = std::wstring(current) + L"/plugins/*.dll";
+    HANDLE hFile = FindFirstFile(PluginPath.c_str(), &FindFile);
 
-	if (hFile == INVALID_HANDLE_VALUE) {
-		std::cerr << "plugins not found" << std::endl;
-		return;
-	}
+    if (hFile == INVALID_HANDLE_VALUE) {
+        std::cerr << "plugins not found" << std::endl;
+        return;
+    }
 
-	do {
-		std::wstring Full = std::wstring(current) + L"/plugins/" + std::wstring(FindFile.cFileName);
-		HMODULE Module = LoadLibrary(Full.c_str());
-		if (Module) {
-			FuncPtr func = (FuncPtr)GetProcAddress(Module, "operationE");
-			FuncNamePtr name = (FuncNamePtr)GetProcAddress(Module, "getNameE");
-			FuncArityPtr arity = (FuncArityPtr)GetProcAddress(Module, "getArityE");
+    do {
+        std::wstring Full = std::wstring(current) + L"/plugins/" + std::wstring(FindFile.cFileName);
+        tryLoadPlugin(Full, registry);
 
-			if (func && name && arity) {
-				std::string namefunc = name();
-				int arityfunc = arity();
+    } while (FindNextFile(hFile, &FindFile) != 0);
 
-				registry.addOper(std::make_unique<PluginAdapter>(namefunc, func, arityfunc));
-				std::cout << "loaded " << namefunc << " arity " << arityfunc << std::endl;
-			}
-			else {
-				std::wcerr << L"not load function from " << Full.c_str() << L" invalid DLL" << std::endl;
-				FreeLibrary(Module);
-			}
-		}
-		else {
-			std::wcerr << L"not load function " << Full.c_str() << std::endl;
-		}
-
-	} while (FindNextFile(hFile, &FindFile) != 0);
-
-	FindClose(hFile);
+    FindClose(hFile);
 }
